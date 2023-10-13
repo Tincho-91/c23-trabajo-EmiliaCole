cb95f43547b0a19c234bb001b755d94464d8a431
Tincho-91 <martin_baryuk@outlook.com>
2023-10-13 19:57:49 -0300

    terminamos estructura html

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const app = express();
 const path = require("path");
 
 app.listen(3000,()=>{
-    console.log("servidor ejecutandose en el puerto 300")
+    console.log("servidor ejecutandose en el puerto 3000")
 })
 
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"));
+});
+
+app.get("/about",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/about.html"));
+})
+
+app.get("/music",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/music.html"));
+})
+
+app.get("/contact",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/contact.html"));
 })
 
 app.use(express.static("public"));
